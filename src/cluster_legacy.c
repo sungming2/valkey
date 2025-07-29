131d9025ad3c495d429de8d8c3cbb16f6f3a4371
Seungmin Lee <sungming@amazon.com>
2025-07-28 21:36:05 -0700

    4 byte timestamp

--- src/cluster_legacy.c
+++ src/cluster_legacy.c
@@ -1684,7 +1684,7 @@ clusterNode *createClusterNode(char *nodename, int flags) {
 }
 
 /* 8 bytes mstime + 8 bytes cluster node pointer (padded with zeros on 32-bit systems). */
-#define FAILURE_REPORT_KEYLEN 16
+#define FAILURE_REPORT_KEYLEN 12
 #define SEC_IN_MS 1000ULL
 #define CEIL_OFFSET_MS (SEC_IN_MS - 1)
 
@@ -1695,24 +1695,28 @@ static void encodeFailureReportKey(clusterNode *node, mstime_t report_time, unsi
     const size_t node_ptr_pad_bytes = (sizeof(clusterNode *) == 4) ? 4 : 0; // pad on 32-bit
 
     /* Round up to the next second for fewer key splits and quorum grace */
-    mstime_t bucketed_time = (report_time / SEC_IN_MS) * SEC_IN_MS + SEC_IN_MS;
+    /* 1) Bucket to the next second for quorum grace */
+    mstime_t bucketed_ms = (report_time / SEC_IN_MS) * SEC_IN_MS + SEC_IN_MS;
+    uint32_t secs = (uint32_t)(bucketed_ms / SEC_IN_MS);
 
     /* Big endian timestamp for numeric time order in rax */
-    uint64_t big_endian_time = htonu64((uint64_t)bucketed_time);
-    memcpy(buf_out, &big_endian_time, sizeof(uint64_t));
+    uint32_t be_secs = htonl(secs);
+    memcpy(buf_out, &be_secs, sizeof(uint32_t));
 
     /* Append the node pointer, plus padding if necessary */
-    memcpy(buf_out + sizeof(uint64_t), &node, sizeof(node));
-    if (node_ptr_pad_bytes) memset(buf_out + sizeof(uint64_t) + sizeof(node), 0, node_ptr_pad_bytes);
+    memcpy(buf_out + sizeof(uint32_t), &node, sizeof(node));
+    if (node_ptr_pad_bytes) memset(buf_out + sizeof(uint32_t) + sizeof(node), 0, node_ptr_pad_bytes);
 }
 
 /* Reverses the operation of encodeFailureReportKey, reading back a timestamp
  * and the pointer to the associated clusterNode. */
 static void decodeFailureReportKey(unsigned char *buf, mstime_t *report_time, clusterNode **node_ptr) {
-    uint64_t big_endian_time;
-    memcpy(&big_endian_time, buf, sizeof(uint64_t));
-    *report_time = (mstime_t)ntohu64(big_endian_time);
-    memcpy(node_ptr, buf + sizeof(uint64_t), sizeof(*node_ptr));
+    uint32_t be_secs;
+    memcpy(&be_secs, buf, sizeof(be_secs));
+    uint32_t secs = ntohl(be_secs);
+    *report_time = (mstime_t)secs * SEC_IN_MS;
+
+    memcpy(node_ptr, buf + sizeof(uint32_t), sizeof(*node_ptr));
 }
 
 /* This function is called every time we get a failure report from a node.
